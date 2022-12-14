af264fd10c1b6634056d0aea4f762b64d1c16324
Victor Rodas <victormatiasrodas100@gmail.com>
2022-12-14 19:03:42 -0300

    message changed for route not found

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import v1TasksRoutes from './routes/v1/tasks.routes.js'
-import v2Routes from './routes/v2/base.routes.js'
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
+import { db } from './configs/db.config.js'
 import authRoutes from './routes/auth.routes.js'
 import { logger } from './middlewares/logger.js'
-import { db } from './configs/db.config.js'
-import * as dotenv from 'dotenv'
-import express from 'express'
-import cookieParser from 'cookie-parser'
+import v2Routes from './routes/v2/base.routes.js'
+import v1TasksRoutes from './routes/v1/tasks.routes.js'
 
 
 const app = express()
@@ -26,9 +26,9 @@ app.use('/api', authRoutes)
 app.use('/api/v1', v1TasksRoutes)
 app.use('/api/v2', v2Routes)
 
-app.use((_req, res) => {
+app.use((req, res) => {
     res.status(404).json({
-        error: 'Route not found!'
+        error: `Cannot ${req.method} ${req.url}`
     })
 })
 
