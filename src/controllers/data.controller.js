ca8f3dd82ac7920b182343d60681c05c6d69d135
Victor Rodas <victormatiasrodas100@gmail.com>
2022-12-15 12:51:43 -0300

    adding operations for user tasks

--- src/controllers/data.controller.js
+++ src/controllers/data.controller.js
@@ -1,12 +1,78 @@
-export const getTasks = (req, res) => {
-    console.log(req.session)
+import { Task } from '../models/Task.js'
+import { findBy } from '../services/general.services.js'
+import { createTask } from '../services/tasks.services.js'
+import { validateTaskFields, validateReqId } from '../utils/validator.task.js'
 
-    res.json({
-        status: 'OK',
-        response: 'Hello, World!'
+export const getTasks = async (req, res) => {
+    const { id, name, email } = req.session
+    const tasks = await findBy(Task, { user_id: id }, { all: true })
+    const completedTasks = tasks.filter(task => task.completed)   
+
+    return res.json({
+        requestedAt: new Date(),
+        username: `${name}`,
+        email,
+        totalTasks: tasks.length,
+        totalCompleted: completedTasks.length,
+        tasks
     })
 }
 
-export const getTaskById = (req, res) => {
-    console.log('TODO: get task by id')
+export const getTaskById = async (req, res) => {
+    try {
+        const id = validateReqId(req.params.id)
+        const task = await findBy(Task, { id })
+
+        if(!task) {
+            return res.status(404).json({
+                message: `Task with id ${id} not found`
+            })
+        }
+
+        if('error' in task) {
+            return res.status(503).json({
+                error: 'Database Error'
+            })
+        }
+
+        return res.json({ task })
+        
+    } catch (error) {
+        res.status(400).json({
+            error: error.message
+        })
+    }
+}
+
+export const createTasks = async (req, res) => {
+    try {
+        const { id } = req.session
+        const { title, description, completed } = validateTaskFields(req.body)
+
+        const { code, message } = await createTask({
+            title,
+            description,
+            completed,
+            user_id: id
+        })
+
+        return res.status(code).json({
+            message,
+            createdAt: new Date()
+        })
+        
+    } catch (error) {
+        res.status(400).json({
+            error: error.message
+        })
+    }
+}
+
+export const updateTask = async (req, res) => {
+    const { id } = req.params //TODO: validate id
+    res.send('TODO: update task')
+}
+
+export const deleteTask = async (req, res) => {
+    res.send('TODO: delete task')
 }
--- src/routes/v1/tasks.routes.js
+++ src/routes/v1/tasks.routes.js
@@ -1,16 +1,19 @@
-import { Router } from 'express';
-import { getTasks, getTaskById } from '../../controllers/data.controller.js';
-import { guard } from '../../middlewares/guard.js';
+import { Router } from 'express'
+import { guard } from '../../middlewares/guard.js'
+import {
+    getTasks, getTaskById,
+    createTasks, updateTask,
+    deleteTask
+} from '../../controllers/data.controller.js'
 
 const router = Router()
 
-//TODO: make guards for every route
 router
     .get('/tasks', guard, getTasks)
-    .get('/tasks/:taskId', getTaskById)
-    .post('/tasks')
-    .put('/tasks/:taskId')
-    .delete('/tasks/:taskId')
+    .get('/tasks/:id', guard, getTaskById)
+    .post('/tasks', guard, createTasks)
+    .put('/tasks/:id', guard, updateTask)
+    .delete('/tasks/:id', guard, deleteTask)
 
 
 export default router
