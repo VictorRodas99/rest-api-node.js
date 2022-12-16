4568d8a81dd3b237df643d0fa42e5582fe86c528
Victor Rodas <victormatiasrodas100@gmail.com>
2022-12-16 15:33:15 -0300

    added function to sort task fields

--- src/utils/general.tools.js
+++ src/utils/general.tools.js
@@ -7,6 +7,25 @@ export const isBoolean = data => {
 }
 
 
+// function for sorting fields in validator.tasks
+export const sortFields = (unsorted, validOrder) => {
+    const sorted = [...unsorted]
+
+    sorted.sort((a, b) => {
+        const aIndex = validOrder.indexOf(a)
+        const bIndex = validOrder.indexOf(b)
+    
+        if(aIndex < bIndex) return -1
+    
+        if(aIndex > bIndex) return 1
+    
+        return 0
+    })
+
+    return sorted
+}
+
+
 export const getDate = () => {
     const date = new Date()
     const fullDate = date.toLocaleDateString()
