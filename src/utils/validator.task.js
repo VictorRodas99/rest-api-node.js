1f5a7c1a115895a5e02b5d5aa87481018d4f19e2
Victor Rodas <victormatiasrodas100@gmail.com>
2022-12-16 15:39:10 -0300

    added option to ignore certain fields

--- src/utils/validator.task.js
+++ src/utils/validator.task.js
@@ -1,4 +1,4 @@
-import { isString, isBoolean } from './general.tools.js'
+import { isString, isBoolean, sortFields } from './general.tools.js'
 
 const parseTitle = dirtyTitle => {
     if(!isString(dirtyTitle)) {
@@ -22,7 +22,7 @@ const parseDescription = dirtyDescription => {
 
 const parseCompleted = dirtyCompleted => {
     if(!isBoolean(dirtyCompleted)) {
-        throw new Error('Completed field only accepts "true" or "false"')
+        throw new Error("Completed field only accepts 'true' or 'false'")
     }
 
     return dirtyCompleted
@@ -31,10 +31,40 @@ const parseCompleted = dirtyCompleted => {
 
 /**
  * 
- * @param { any } dirtyData 
- * @returns { { title: string, description: string, completed: boolean } } taskData
+ * @param { any } dirtyData
+ * @param { { allFields: boolean } } [options = { allFields: true }] - Set true if you want to validate all the defined fields
+ * @returns { { title?: string, description?: string, completed?: boolean } } taskData
  */
-export const validateTaskFields = dirtyData => {
+export const validateTaskFields = (dirtyData, options = { allFields: true }) => {
+    if(!dirtyData) {
+        throw new Error('Invalid data!')
+    }
+
+    if(!options.allFields) {
+        const data = {}
+        const validFields = ['title', 'description', 'completed'] // Valid fields order
+        const validators = [parseTitle, parseDescription, parseCompleted]
+
+        const fields = Object.keys(dirtyData).filter(field => validFields.includes(field))
+
+        if(fields.length <= 0) {
+            throw new Error("Invalid fields! (Valid: 'name', 'description', 'completed'")
+        }
+
+        const sortedFields = sortFields(fields, validFields)
+        const values = sortedFields.map(field => dirtyData[field])
+
+        values.forEach((value, index) => {
+            const field = sortedFields[index]
+
+            const indexValidator = validFields.indexOf(field)
+            const validator = validators[indexValidator]
+
+            data[field] = validator(value)
+        })
+
+        return data
+    }
 
     const data = {
         title: parseTitle(dirtyData.title),
